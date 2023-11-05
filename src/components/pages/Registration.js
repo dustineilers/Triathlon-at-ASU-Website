f2dc45cf84ccb1bf947eca347f943c2875dba38e
= <eilersd23@gmail.com>
2023-11-05 14:53:21 -0500

    Create Registration page

---
+++ src/components/pages/Registration.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../App.css";
+import Footer from "../Footer";
+
+export default function Registration() {
+  return (
+    <>
+      <div className="registration">
+        <h1>REGISTRATION</h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
